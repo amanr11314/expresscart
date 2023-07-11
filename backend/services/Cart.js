85242c399dbb1ad4596bb4e483b80651c2939940
Aman Raj <amanr11314@gmail.com>
2023-07-11 20:27:05 +0530

    wip: cart add flow

---
+++ backend/services/Cart.js
@@ -0,0 +1,26 @@
+const db = require('../models')
+
+exports.getCartDetails = (callback) => {
+    let cart = { products: [] }
+    return callback(cart)
+}
+
+exports.addProductToCart = (productId, price) {
+    // add product to loggedIn user's cart
+    // https://github.com/leelanarasimha/node-express-app/blob/main/models/Cart.js
+    this.getCartDetails((cart) => {
+        let existingProductIdx = cart.products.findIndex((prod) => prod.id.toString() === productId)
+        let updatedProduct;
+
+        if (existingProductIdx !== -1) {
+            updatedProduct = { ...cart.products[existingProductIdx] }
+            updatedProduct.quantity += 1;
+            cart.products = [...cart.products]
+            cart.products[existingProductIdx] = updatedProduct;
+        } else {
+            updatedProduct = { id: productId, quantity: 1 }
+            cart.products = [...cart.products, updatedProduct]
+        }
+
+    })
+}
