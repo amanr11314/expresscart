50e91c9236189edff7f97884ccd09d319448c36b
Aman Raj <amanr11314@gmail.com>
2023-07-11 19:34:50 +0530

    CRUD on sequelize Product model

--- backend/services/Product.js
+++ backend/services/Product.js
@@ -1,15 +1,9 @@
 const db = require('../models')
-// sequelize.query(
-//     'SELECT * FROM students WHERE id = :id',
-//     {
-//         replacements: { id: 2 },
-//         type: sequelize.QueryTypes.SELECT
-//     }
-// ).then(result => {
-//     console.log(result);
-// }).catch((error) => {
-//     console.error('Failed to insert data : ', error);
-// });
+
+exports.saveProduct = (product) => {
+    // Create a new Product
+    return db.Product.create(product);
+}
 
 exports.fetchAllProducts = () => {
     return db.Product.findAll();
@@ -22,7 +16,6 @@ exports.getProductById = (productId) => {
         }
     })
 }
-const sample = { "id": "1", "title": "Apple iPhone 14 Pro Plus", "description": "iOS14 with A22 bionic chip", "price": 199999, "imgUrl": null, "createdAt": "2023-07-11T07:34:34.402Z", "updatedAt": "2023-07-11T07:34:34.402Z" };
 
 exports.updateProductById = (product, productId) => {
     return db.Product.update(product, {
@@ -32,7 +25,31 @@ exports.updateProductById = (product, productId) => {
     })
 }
 
-// db.User.findAll().then((data) => console.log(JSON.stringify(data)))
-this.getProductById(10)
-    .then((data) => console.log(JSON.stringify(data)))
-    .catch((err) => console.log(`erorr not found ${err}`))
+exports.deleteProductById = (productId) => {
+    return db.Product.destroy({
+        where: {
+            id: productId
+        }
+    })
+}
+
+// // db.User.findAll().then((data) => console.log(JSON.stringify(data)))
+// const newProduct = {
+//     title: 'Nothing Phone 2',
+//     description: 'New Nothing Phone with awesome features',
+//     price: 89000,
+//     createdAt: "2023-07-11T07:34:34.402Z",
+//     updatedAt: "2023-07-11T07:34:34.402Z",
+// }
+
+// this.deleteProductById(1)
+//     .then((data) => console.log(JSON.stringify(data)))
+//     .catch((err) => console.log(`erorr not found ${err}`))
+
+// this.saveProduct(newProduct)
+//     .then((data) => console.log(JSON.stringify(data)))
+//     .catch((err) => console.log(`erorr not found ${err}`))
+
+// this.getProductById(10)
+//     .then((data) => console.log(JSON.stringify(data)))
+//     .catch((err) => console.log(`erorr not found ${err}`))
