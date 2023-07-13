dc920b92b50c534836599f02870cd93054f34e24
Aman Raj <amanr11314@gmail.com>
2023-07-13 19:36:11 +0530

    fix: enable cors on server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const express = require('express')
 const rootDir = require('./backend/utils/path')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express();
 const backend = require('./backend/core')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(backend)
