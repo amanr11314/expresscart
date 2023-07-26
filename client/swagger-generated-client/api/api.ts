55129cb85ea68b2f5df82ba92881f39d6cb9731d
Aman Raj <amanr11314@gmail.com>
2023-07-26 18:05:16 +0530

    chore: delete generated client from main repo

--- client/swagger-generated-client/api/api.ts
+++
@@ -1,3 +0,0 @@
-export * from './productCRUDOperations.service';
-import { ProductCRUDOperationsService } from './productCRUDOperations.service';
-export const APIS = [ProductCRUDOperationsService];
--- client/swagger-generated-client/model/models.ts
+++
@@ -1,7 +0,0 @@
-export * from './createProductRequest';
-export * from './deleteProductRequest';
-export * from './editProductRequest';
-export * from './modelError';
-export * from './product';
-export * from './successNoContent';
-export * from './unauthorizedError';
